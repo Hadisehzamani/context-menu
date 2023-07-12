2612016a319eaa110897cfeae65071a8e6879a87
Hadisehzamani <hadisehzmn@gmail.com>
2023-07-12 18:37:28 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+﻿var context_menu = document.querySelector('.context-menu')
+
+
+document.body.addEventListener('contextmenu', function(event){
+    event.preventDefault()
+    context_menu.style.left = event.pageX
+    context_menu.style.top = event.pageY
+    context_menu.style.display = 'block'
+    document.body.addEventListener('keydown', function(event){
+        if(event.keyCode == 27){
+            context_menu.style.display = 'none'
+        }
+    })
+    document.body.addEventListener('click', function(){
+        context_menu.style.display = 'none'
+    })
+})
